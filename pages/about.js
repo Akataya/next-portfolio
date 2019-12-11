72af633eac45c0c14d3a9a404490152df1d01877
Akataya <infotechpro@yahoo.com>
2019-12-11 18:37:54 -0500

    Created initial pages, Layout component and loading spinner

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import Layout from "../components/Layout";
+
+export default () => (
+  <Layout title="About">
+    <p>A JavaScript programmer.</p>
+    <img src="/static/jslogo.png" alt="JavaScript" height="200px" />
+  </Layout>
+);
---
+++ pages/hireme.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+const HireMe = () => (
+  <Layout title="Hire me">
+    <p>
+      You can hire me at{" "}
+      <a href="mailto:nycwebpro@gmail.com">nycwebpro@gmail.coms</a>
+    </p>
+  </Layout>
+);
+
+export default HireMe;
